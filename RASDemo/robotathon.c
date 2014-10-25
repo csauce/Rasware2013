7c94b139b61bf6d32e15cc63428fa1f376f6a95e
Christopher Sauceda <csauce@utexas.edu>
2014-10-24 22:10:25 -0500

    Updated wall following (frame for the actual code)
    Robotathon project was removed because the uvproj target and other
    configs are difficult to set in a brand new project so we are keeping
    the RASdemo uvproj  for that simple fact. All of the RASdemo files
    should be removed from the uvproj (but are still in the folder) and
    robotathon.c contains the main method for the (now wall following but
    soon to be full event) code.

--- RASDemo/robotathon.c
+++ RASDemo/robotathon.c
@@ -1,32 +1,42 @@
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/adc.h>
+#include <RASLib/inc/motor.h>
 
-static tADC *adc[1];
+//Right now we are just doing the wall following code - Chris Sauceda 10/24/2014 7:56 PM
+//2 IR sensors will be needed (for addressing the corners of course)
+static tADC *adc;
 static tBoolean initialized = false;
+static tMotor *motors[2]; //driving servos left motor - 0 right motor - 1
 
-void initIRSensor(void) {
+void initDevices(void) 
+{
     // don't initialize this if we've already done so
-    if (initialized) {
+    if (initialized) 
+		{
         return;
     }
     
     initialized = true;
 
-    // initialize 4 pins to be used for ADC input
-    adc[0] = InitializeADC(PIN_D1);
+    // initialize 2 pins to be used for ADC input
+    adc = InitializeADC( PIN_D0 ); //again, side IR sensor
+		
+		motors[0] = InitializeServoMotor(PIN_B3, false); //left motor 
+		motors[1] = InitializeServoMotor(PIN_C4, true); //right motor
+		
 }
 
-int main(void) {
-		initIRSensor();
-    Printf("Press any key to quit\n");
-        
-    // loop as long as the user doesn't press a key 
-    while (!KeyWasPressed()) {
-        Printf(
-            "IR values:  %1.3f\r",
-            ADCRead(adc[0])
-            );
-    }
-  
-    Printf("\n");
+int main(void) 
+{
+		initDevices(); //initialize ALL of the components in one fell sweep.
+		/*
+		*
+		* 
+		* Just some testing code to verify motor functionality (it's below)
+		*/
+		while(1)
+		{
+			SetMotor(motors[0], 1.0); 
+			SetMotor(motors[1], 1.0);
+		}
 }
